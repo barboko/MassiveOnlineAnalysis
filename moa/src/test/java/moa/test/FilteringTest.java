275b80a746faf4990bc4746c6193be05b2270e9d
bar boko <bokovza@outlook.co.il>
2016-03-16 09:19:07 +0200

    Fix Debug Issues

--- moa/src/test/java/moa/test/FilteringTest.java
+++ moa/src/test/java/moa/test/FilteringTest.java
@@ -1,7 +1,8 @@
 package moa.test;
 
-import com.yahoo.labs.samoa.instances.Instance;
+import moa.core.Example;
 import moa.streams.MultiTargetArffFileStream;
+import moa.streams.filters.DuplicateFilter;
 import org.junit.Test;
 
 
@@ -9,17 +10,21 @@ import org.junit.Test;
  * @author Bar Bokovza (barboko) & Leonid Rice (leorice)
  */
 public class FilteringTest {
-    //region members
-    private MultiTargetArffFileStream stream;
-    //endregion
-
     @Test
     public void run() {
-        stream = new MultiTargetArffFileStream(ClassLoader.getSystemResource("moa/classifiers/data/small_regression.arff").getPath(), "4-6");
+        MultiTargetArffFileStream stream = new MultiTargetArffFileStream(ClassLoader.getSystemResource("moa/classifiers/data/small_regression.arff").getPath(), "4-6");
 
-        Instance inst = stream.nextInstance().getData();
-        System.out.println(inst.inputAttribute(0).name());
-        inst.deleteAttributeAt(0);
-        System.out.println(inst.inputAttribute(0).name());
+        DuplicateFilter filter = new DuplicateFilter();
+        filter.setInputStream(stream);
+        filter.restart();
+
+        Example example = filter.nextInstance();
+        example = filter.nextInstance();
+        example = filter.nextInstance();
+        example = filter.nextInstance();
+        example = filter.nextInstance();
+        example = filter.nextInstance();
+        example = filter.nextInstance();
+
     }
 }
